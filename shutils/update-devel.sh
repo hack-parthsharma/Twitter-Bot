b2875010096f7768a352df9e9043ef4395f19714
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2022-07-07 08:26:05 +0530

    Add files via upload

---
+++ shutils/update-devel.sh
@@ -0,0 +1,50 @@
+#
+# twitterbot2
+#
+#   edoardottt
+#   edoardoottavianelli.it
+#   https://github.com/edoardottt/twitterbot2
+#
+#   This repository is under GPL-3 License.
+#
+# This file is intended to update the devel branch
+# on the test machine to quickly test the new 
+# functionalities/fixes.
+# The database.db, config.yaml and twitterbot2.log files
+# will be kept. 
+#
+
+if [ -f database.db ]
+then 
+    cp database.db ..
+fi 
+
+if [ -f config.yaml ]
+then 
+    cp config.yaml ..
+fi 
+
+if [ -f twitterbot2.log ]
+then 
+    cp twitterbot2.log ..
+fi 
+
+cd ..
+rm -rf twitterbot2
+git clone --single-branch --branch devel https://github.com/edoardottt/twitterbot2
+cd twitterbot2
+
+if [ -f ../database.db ]
+then 
+    cp ../database.db .
+fi 
+
+if [ -f ../config.yaml ]
+then 
+    cp ../config.yaml .
+fi 
+
+if [ -f ../twitterbot2.log ]
+then 
+    cp ../twitterbot2.log .
+fi 
